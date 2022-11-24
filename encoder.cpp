d243960459d8406be56b17c08e80b83b3d81bc96
Bagdaulet02 <qadyr.bagdaulet@gmail.com>
2022-11-24 14:51:33 +0600

    Keiin ayaqtaimyn

---
+++ encoder.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+class alphabet {
+private:
+  char abc[2][3][10];
+  abc[0][0][0] = '';
+  abc[0][1] = ''
+
+public:
+  alphabet (arguments);
+  virtual ~alphabet ();
+};
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
